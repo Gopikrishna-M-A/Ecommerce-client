6ac9ea162b62359b86212393521d7e6f926cb27a
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-01-19 11:34:22 +0530

    fix: resolve banner image blurriness issue

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState, useEffect } from "react";
 import {
   MenuOutlined,
@@ -15,33 +15,24 @@ import Image from "next/image";
 
 const { Title, Text } = Typography;
 
-const contentStyle = {
-  height: "300px",
-  color: "#363636",
-  lineHeight: "300px",
-  textAlign: "center",
-  background: "#dcdcdc",
-};
-
 const Home = ({ baseURL }) => {
+  const [categories, setCategories] = useState([]);
 
-    const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    axios.get(`${baseURL}/api/products`).then((res) => {
+      const uniqueCategories = Object.values(
+        res.data.reduce((acc, product) => {
+          const categoryId = product.category._id;
+          if (!acc[categoryId]) {
+            acc[categoryId] = product.category;
+          }
+          return acc;
+        }, {})
+      );
 
-    useEffect(() => {
-        axios.get(`${baseURL}/api/products`).then((res) => {
-            const uniqueCategories = Object.values(
-                res.data.reduce((acc, product) => {
-                  const categoryId = product.category._id;
-                  if (!acc[categoryId]) {
-                    acc[categoryId] = product.category;
-                  }
-                  return acc;
-                }, {})
-              );
-            
-            setCategories(uniqueCategories)
-        });
-    }, []);
+      setCategories(uniqueCategories);
+    });
+  }, []);
 
   return (
     <div className="px-10 py-2.5">
@@ -95,38 +86,38 @@ const Home = ({ baseURL }) => {
           <Carousel className="carousel" autoplay>
             <div>
               <Image
-                width={100}
-                height={100}
+                width={816}
+                height={320}
                 src="/images/Banner/1.png"
-                alt="Image 1"
-                style={{ width: "100%", height: "100%" }}
+                alt="Banner Image"
+                className="w-full"
               />
             </div>
             <div>
               <Image
-               width={100}
-               height={100}
+                width={816}
+                height={320}
                 src="/images/Banner/2.png"
-                alt="Image 1"
-                style={{ width: "100%", height: "100%" }}
+                alt="Banner Image"
+                className="w-full"
               />
             </div>
             <div>
               <Image
-              height={100}
-               width={100}
+                width={816}
+                height={320}
                 src="/images/Banner/3.png"
-                alt="Image 1"
-                style={{ width: "100%", height: "100%" }}
+                alt="Banner Image"
+                className="w-full"
               />
             </div>
             <div>
               <Image
-               width={100}
-               height={100}
+                width={816}
+                height={320}
                 src="/images/Banner/4.png"
-                alt="Image 1"
-                style={{ width: "100%", height: "100%" }}
+                alt="Banner Image"
+                className="w-full"
               />
             </div>
           </Carousel>
@@ -165,9 +156,7 @@ const Home = ({ baseURL }) => {
           </Text>
         </div>
 
-        <div
-          className="flex gap-2 mt-2.5 overflow-x-auto py-2.5 px-2.5"
-        >
+        <div className="flex gap-2 mt-2.5 overflow-x-auto py-2.5 px-2.5">
           {categories.map((category) => {
             return <CategoryCard key={category._id} category={category} />;
           })}
